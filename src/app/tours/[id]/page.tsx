9d3f7ce90c3272bf3bc80cf00141200bdd820e23
Hamidi Anes <aneshamididev@gmail.com>
2025-08-31 17:15:14 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/tours/[id]/page.tsx
+++ src/app/tours/[id]/page.tsx
@@ -5,7 +5,7 @@ import { TourExecutionClient } from "@/components/tours/TourExecutionClient";
 
 export default function TourExecutionPage({ params }: { params: { id: string } }) {
   return (
-    <div className="h-[calc(10H-3.5rem)]">
+    <div className="h-[calc(100vh-3.5rem)]">
       <TourExecutionClient tourId={params.id} />
     </div>
   );
